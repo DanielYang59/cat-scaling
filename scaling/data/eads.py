a4de5e4ef7667f17e1e78932bd6eb88f5f9a3efa
Haoyu Yang <yanghaoyu97@outlook.com>
2024-02-27 19:01:06 +0800

    update Eads docstring and test

--- scaling/data/eads.py
+++ scaling/data/eads.py
@@ -5,7 +5,26 @@ import pandas as pd
 
 
 class Eads:
+    """Handle adsorption energies for linear scaling relations.
+
+    Expect data as a pd.DataFrame in the following format:
+                   *CO2   *COOH  ...  *OCH3  *O     *OH
+        Cu@g-C3N4  0.89   4.37   ...  3.98  -1.73   0.17
+        Ni@C2N    -4.57  -4.95  ...  -0.93  -2.81  -3.21
+        ...
+        Pt@SiO2   -2.36   3.69   ...  3.12   0.29   4.84
+        Au@Al2O3   2.15  -2.35  ...   1.36   1.07   4.56
+
+    Note:
+        - Column headers (0th row) should be adsorbate names.
+        - Row headers (0th column) should be sample names.
+
+    Attributes:
+        df (pd.DataFrame): The DataFrame containing adsorption energy data.
+    """
+
     def __init__(self, df: pd.DataFrame) -> None:
+        """Initialize the Eads class with a DataFrame."""
         # Check and take arg: df
         if isinstance(df, pd.DataFrame):
             try:
@@ -17,6 +36,9 @@ class Eads:
             raise TypeError("Expect data as pd.DataFrame type.")
 
     def get_adsorbates(self) -> list[str]:
+        """Get adsorbate names from column headers."""
         return self.df.columns.values.tolist()
 
-
+    def get_samples(self) -> list[str]:
+        """Get sample names from row headers."""
+        return self.df.index.tolist()
--- tests/data/test_eads.py
+++ tests/data/test_eads.py
@@ -16,10 +16,20 @@ class Test_eads:
 
         assert isinstance(eads.df, pd.DataFrame)
 
-    def test_get_adsorbate(self):
+    def test_get_adsorbates(self):
         test_df = pd.read_csv(self.test_data_csv, index_col=[0], header=[0])
         eads = Eads(test_df)
 
         adsorbates = eads.get_adsorbates()
 
         assert adsorbates == ["*CO2", "*COOH", "*CO", "*OCH3", "*O", "*OH"]
+
+    def test_get_samples(self):
+        test_df = pd.read_csv(self.test_data_csv, index_col=[0], header=[0])
+        eads = Eads(test_df)
+
+        samples = eads.get_samples()
+
+        assert samples == [
+            "Cu@g-C3N4", "Ni@C2N", "Fe@BN", "Co@BN", "Pt@SiO2", "Au@Al2O3"
+        ]
