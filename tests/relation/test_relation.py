33bf76bc07480ced449c09aed6d918d5e4a3293a
Haoyu Yang <yanghaoyu97@outlook.com>
2024-02-29 11:26:58 +0800

    add tests for relation

---
+++ tests/relation/test_relation.py
@@ -0,0 +1,61 @@
+
+import pytest
+from scaling.relation import Relation
+
+
+class Test_relation:
+    def test_init(self):
+        test_coef = {
+            "a": [1, 2, 3],
+            "b": [0.1, 0.2, 0.3],
+        }
+        relation = Relation(test_coef)
+
+        assert relation.dim == 2
+
+    def test_invalid_data_dtype(self):
+        invalid_coef = []
+        with pytest.raises(TypeError) as excinfo:
+            Relation(invalid_coef)
+
+            assert excinfo.value == "Coefficients must be a dictionary"
+
+    def test_invalid_species_name(self):
+        invalid_coef = {
+            1: [1, 2, 3],
+            "b": [0.1, 0.2, 0.3],
+        }
+        with pytest.raises(TypeError) as excinfo:
+            Relation(invalid_coef)
+
+            assert excinfo.value == "Species name must be strings"
+
+    def test_invalid_coef_type(self):
+        invalid_coef = {
+            "a": "1",
+            "b": [0.1, 0.2, 0.3],
+        }
+        with pytest.raises(TypeError) as excinfo:
+            Relation(invalid_coef)
+
+            assert excinfo.value == "Input coefficients must be lists"
+
+    def test_invalid_coef_dtype(self):
+        invalid_coef = {
+            "a": ["1", "2"],
+            "b": [0.1, 0.2, 0.3],
+        }
+        with pytest.raises(TypeError) as excinfo:
+            Relation(invalid_coef)
+
+            assert excinfo.value == "Coefficients must be floats"
+
+    def test_coef_length_mismatch(self):
+        invalid_coef = {
+            "a": [1, 2],
+            "b": [0.1, 0.2, 0.3],
+        }
+        with pytest.raises(ValueError) as einfo:
+            Relation(invalid_coef)
+
+            assert einfo.value == "All coefficients must have the same length"
