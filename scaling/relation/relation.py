d7768706b459941fd12502d040b2cc606e8b5c9c
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-05 11:33:48 +0800

    fix line broken by ruff

--- scaling/relation/relation.py
+++ scaling/relation/relation.py
@@ -35,24 +35,14 @@ class Relation:
 
     def __init__(
         self,
-        coefficients: dict[
-            str,
-            list[float],
-        ],
-        metrics: Optional[
-            dict[
-                str,
-                float,
-            ]
-        ] = None,
+        coefficients: dict[str, list[float]],
+        metrics: Optional[dict[str, float]] = None,
     ) -> None:
         """Initialize Relation with coefficients.
 
         Args:
             coefficients (dict[str, list[float]]): Dictionary mapping
                 species names to lists of coefficients.
-            descriptor_manager (Optional[list[str]]): Managing descriptors
-                used for scaling relations.
             metrics (Optional[dict[str, float]]): Evaluation metrics
                 (MAE/R2 or such) of this Relation. Defaults to None.
         """
@@ -62,52 +52,26 @@ class Relation:
         self.metrics = metrics
 
     @property
-    def coefficients(
-        self,
-    ) -> dict[
-        str,
-        list[float],
-    ]:
+    def coefficients(self) -> dict[str, list[float]]:
         """Coefficient matrix."""
 
         return self._coefficients
 
     @coefficients.setter
-    def coefficients(
-        self,
-        coefficients: dict[
-            str,
-            list[float],
-        ],
-    ):
+    def coefficients(self, coefficients: dict[str, list[float]]):
         """Set coefficients and calculate dim."""
 
         # Check data types
-        if not isinstance(
-            coefficients,
-            dict,
-        ):
+        if not isinstance(coefficients, dict):
             raise TypeError("Coefficients must be a dictionary")
 
-        for (
-            key,
-            value,
-        ) in coefficients.items():
-            if not isinstance(
-                key,
-                str,
-            ):
+        for key, value in coefficients.items():
+            if not isinstance(key, str):
                 raise TypeError("Species name must be strings")
-            if not isinstance(
-                value,
-                list,
-            ):
+            if not isinstance(value, list):
                 raise TypeError("Input coefficients must be lists")
             for item in value:
-                if not isinstance(
-                    item,
-                    float,
-                ):
+                if not isinstance(item, float):
                     raise TypeError("Coefficients must be floats")
 
         # Check coefficient length consistency
@@ -118,36 +82,19 @@ class Relation:
         self._coefficients = coefficients
 
     @property
-    def dim(
-        self,
-    ) -> int:
+    def dim(self) -> int:
         """Dimensionality (as number of descriptors)(read-only)."""
 
         return len(next(iter(self.coefficients.values()))) - 1
 
     @property
-    def metrics(
-        self,
-    ) -> Optional[
-        dict[
-            str,
-            float,
-        ]
-    ]:
+    def metrics(self) -> Optional[dict[str, float]]:
         """Evaluation metrics (MAE/R2 or such) of this Relation."""
 
         return self._metrics
 
     @metrics.setter
-    def metrics(
-        self,
-        metrics: Optional[
-            dict[
-                str,
-                float,
-            ]
-        ],
-    ):
+    def metrics(self, metrics: Optional[dict[str, float]]):
         """Set metrics, which is expect to be a "species: error" dict,
         for example:
             metrics = {
@@ -158,17 +105,11 @@ class Relation:
 
         # Check data types
         if metrics is not None:
-            if not isinstance(
-                metrics,
-                dict,
-            ):
+            if not isinstance(metrics, dict):
                 raise TypeError("metric should be a dict.")
 
             for value in metrics.values():
-                if not isinstance(
-                    value,
-                    float,
-                ):
+                if not isinstance(value, float):
                     raise TypeError("metric values should be float.")
 
             self._metrics = metrics
