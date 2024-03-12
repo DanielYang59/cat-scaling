3c63f34c5341a714269495877438749120069124
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-12 18:43:21 +0800

    merge EadsRelation and RelationBase

--- scaling/relation/relation.py
+++ scaling/relation/relation.py
@@ -1,8 +1,6 @@
-# TODO: unit test needs update to test properties
+# TODO: unit test needs update to test properties and DeltaERelation
 
 # TODO: module docstring needs update
-
-# TODO: merge EadsRelation with RelationBase
 
 """Describe linear scaling relations with a coefficient matrix.
 
@@ -28,41 +26,41 @@ Coefficient matrix:
     Eads X  =  aX                 bX             .......    cX
 """
 
-
 from math import isclose
 
 import numpy as np
 
 
-class RelationBase:
-    """Base Class for describing linear scaling relations
-    with a coefficient matrix.
+class EadsRelation:
+    """Describe linear scaling relations with a coefficient matrix.
 
     Attributes:
         coefficients (dict[str, list[float]]): Dict mapping
             species names to coefficients.
         intercepts (dict[str, float]): Dict mapping
             species names to intercept.
-        dim (float): Dimensionality as the number of descriptors.
+        metrics (dict[str, float]): Evaluation metrics
+            (MAE/R2 or such) of this Relation, for each species.
+        ratios (dict[str, dict[str, float]]): The keys
+            are species names and the values are lists of
+            mixing ratios corresponding to each descriptor.
+        dim (int): Dimensionality as the number of descriptors.
     """
 
     def __init__(
         self,
         coefficients: dict[str, list[float]],
         intercepts: dict[str, float],
+        metrics: dict[str, float],
+        ratios: dict[str, dict[str, float]],
     ) -> None:
-        """Initialize with coefficients and intercepts.
-
-        Args:
-            coefficients (dict[str, list[float]]): Dictionary mapping
-                species names to lists of coefficients.
-            intercepts (dict[str, float]): Dictionary mapping
-                species names to intercept.
-        """
+        """Initialize with coefficients and intercepts."""
 
         # Set properties
         self.coefficients = coefficients
         self.intercepts = intercepts
+        self.metrics = metrics
+        self.ratios = ratios
 
     def __str__(self) -> str:
         """String representation of the Relation."""
@@ -82,6 +80,16 @@ class RelationBase:
             string += f"{ads:<10}"
             string += " ".join([f"{i:<10.2f}" for i in self.coefficients[ads]])
             string += f"{self.intercepts[ads]:<10.2f}\n"
+
+        # Add metrics
+        string += "\nAdsorbate Metrics\n"
+        for name, metric in self.metrics.items():
+            string += f"{name:<10}{metric:<10.2f}\n"
+
+        # Add ratios
+        string += "\nAdsorbate Ratios\n"
+        for name, ratios in self.ratios.items():
+            string += f"{name:<10}{ratios}\n"
 
         return string
 
@@ -139,63 +147,6 @@ class RelationBase:
         """Dimensionality (as number of descriptors)."""
 
         return len(next(iter(self.coefficients.values())))
-
-
-class EadsRelation(RelationBase):
-    """Superclass for describing adsorption (free) energy Relation.
-
-    Attributes:
-        coefficients (dict[str, list[float]]): Dict mapping
-            species names to coefficients.
-        intercepts (dict[str, float]): Dict mapping
-            species names to intercepts.
-        metrics (dict[str, float]): Evaluation metrics
-            (MAE/R2 or such) of each species.
-        ratios (dict[str, dict[str, float]]): The keys
-            are species names and the values are dict of
-            mixing ratios corresponding to each descriptor.
-    """
-
-    def __init__(
-        self,
-        coefficients: dict[str, list[float]],
-        intercepts: dict[str, float],
-        metrics: dict[str, float],
-        ratios: dict[str, dict[str, float]],
-    ) -> None:
-        """Initialize EadsRelation with coefficients.
-
-        Args:
-            coefficients (dict[str, list[float]]): Dictionary mapping
-                species names to lists of coefficients.
-            intercepts (dict[str, float]): Dictionary mapping
-                species names to intercept.
-            metrics (dict[str, float]): Evaluation metrics
-                (MAE/R2 or such) of this Relation.
-            ratios (dict[str, dict[str, float]]): The keys
-                are species names and the values are lists of
-                mixing ratios corresponding to each descriptor.
-        """
-
-        super().__init__(coefficients, intercepts)
-        self.metrics = metrics
-        self.ratios = ratios
-
-    def __str__(self) -> str:
-        """String representation."""
-
-        base_str = super().__str__()
-
-        # Add metrics
-        base_str += "\nAdsorbate Metrics\n"
-        for name, metric in self.metrics.items():
-            base_str += f"{name:<10}{metric:<10.2f}\n"
-
-        # Add ratios
-        base_str += "\nAdsorbate Ratios\n"
-        for name, ratios in self.ratios.items():
-            base_str += f"{name:<10}{ratios}\n"
-        return base_str
 
     @property
     def metrics(self) -> dict[str, float]:
--- tests/relation/test_relation.py
+++ tests/relation/test_relation.py
@@ -3,10 +3,13 @@
 
 import pytest
 
-from scaling.relation.relation import EadsRelation, RelationBase
+from scaling.relation.relation import EadsRelation
 
 
-class Test_RelationBase:
+class Test_EadsRelation:
+    test_metrics = {"a": 0.8, "b": 0.9}
+    test_ratios = {"b": {"a": 1.0}}
+
     def test_init(self):
         test_coef = {
             "a": [0.1, 0.2, 0.3],
@@ -15,7 +18,9 @@ class Test_RelationBase:
 
         test_intercept = {"a": 0, "b": 1}
 
-        relation = RelationBase(test_coef, test_intercept)
+        relation = EadsRelation(
+            test_coef, test_intercept, self.test_metrics, self.test_ratios
+        )
 
         assert relation.dim == len(test_coef["a"])
 
@@ -26,10 +31,16 @@ class Test_RelationBase:
         }
 
         test_intercept = {"a": 0, "b": 1}
+        test_metrics = {"a": 0.8, "b": 0.9}
+        test_ratios = {"b": {"a": 1.0}}
 
-        relation = RelationBase(test_coef, test_intercept)
+        relation = EadsRelation(
+            test_coef, test_intercept, test_metrics, test_ratios
+        )
 
         assert "coef_0     coef_1     coef_2     intercept" in str(relation)
+        assert "Adsorbate Metrics" in str(relation)
+        assert "Adsorbate Ratios" in str(relation)
 
     def test_invalid_data_dtype(
         self,
@@ -37,7 +48,12 @@ class Test_RelationBase:
         invalid_coef = []
         test_intercept = {"a": 0, "b": 1}
         with pytest.raises(TypeError) as excinfo:
-            RelationBase(invalid_coef, test_intercept)
+            EadsRelation(
+                invalid_coef,
+                test_intercept,
+                self.test_metrics,
+                self.test_ratios,
+            )
 
         assert str(excinfo.value) == "Coefficients must be a dictionary"
 
@@ -48,31 +64,43 @@ class Test_RelationBase:
         }
         test_intercept = {"a": 0, "b": 1}
         with pytest.raises(TypeError) as excinfo:
-            RelationBase(invalid_coef, test_intercept)
+            EadsRelation(
+                invalid_coef,
+                test_intercept,
+                self.test_metrics,
+                self.test_ratios,
+            )
 
         assert str(excinfo.value) == "Species name must be strings"
 
     def test_invalid_coef_type(self):
-        invalid_coef = {
+        invalid_coef_0 = {
             "a": "1",
             "b": [0.1, 0.2, 0.3],
         }
         test_intercept = {"a": 0, "b": 1}
         with pytest.raises(TypeError) as excinfo:
-            RelationBase(invalid_coef, test_intercept)
+            EadsRelation(
+                invalid_coef_0,
+                test_intercept,
+                self.test_metrics,
+                self.test_ratios,
+            )
 
         assert str(excinfo.value) == "Input coefficients must be lists"
 
-    def test_invalid_coef_dtype(
-        self,
-    ):
-        invalid_coef = {
+        invalid_coef_1 = {
             "a": ["1", "2"],
             "b": [0.1, 0.2, 0.3],
         }
         test_intercept = {"a": 0, "b": 1}
         with pytest.raises(TypeError) as excinfo:
-            RelationBase(invalid_coef, test_intercept)
+            EadsRelation(
+                invalid_coef_1,
+                test_intercept,
+                self.test_metrics,
+                self.test_ratios,
+            )
 
         assert str(excinfo.value) == "Coefficients must be floats"
 
@@ -83,43 +111,13 @@ class Test_RelationBase:
         }
         test_intercept = {"a": 0, "b": 1}
         with pytest.raises(ValueError) as excinfo:
-            RelationBase(invalid_coef, test_intercept)
+            EadsRelation(
+                invalid_coef,
+                test_intercept,
+                self.test_metrics,
+                self.test_ratios,
+            )
 
         assert (
             str(excinfo.value) == "All coefficients must have the same length"
         )
-
-
-class Test_EadsRelation:
-    def test_init(self):
-        test_coef = {
-            "a": [0.1, 0.2, 0.3],
-            "b": [0.0, 0.1, 0.2],
-        }
-
-        test_intercept = {"a": 0, "b": 1}
-        test_metrics = {"a": 0.8, "b": 0.9}
-        test_ratios = {"b": {"a": 1.0}}
-
-        relation = EadsRelation(
-            test_coef, test_intercept, test_metrics, test_ratios
-        )
-
-        assert relation.dim == len(test_coef["a"])
-
-    def test_str(self):
-        test_coef = {
-            "a": [0.1, 0.2, 0.3],
-            "b": [0.0, 0.1, 0.2],
-        }
-
-        test_intercept = {"a": 0, "b": 1}
-        test_metrics = {"a": 0.8, "b": 0.9}
-        test_ratios = {"b": {"a": 1.0}}
-
-        relation = EadsRelation(
-            test_coef, test_intercept, test_metrics, test_ratios
-        )
-
-        assert "Adsorbate Metrics" in str(relation)
-        assert "Adsorbate Ratios" in str(relation)
