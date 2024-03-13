2a42fb1edd9d6e6105982997a91a2144a46dd58a
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-13 16:32:04 +0800

    for species name in tests

--- tests/relation/test_analysis.py
+++ tests/relation/test_analysis.py
@@ -38,9 +38,9 @@ class Test_AdsorbToDeltaE:
     @pytest.fixture
     def test_reaction(self):
         # Define dummy Species
-        species_A = Species("*A", -1, True)
+        species_A = Species("A", -1, True)
         species_H2O = Species("H2O", -8, False)
-        species_B = Species("*B", -10, True)
+        species_B = Species("B", -10, True)
 
         # Setup a test Reaction
         reaction = Reaction(
